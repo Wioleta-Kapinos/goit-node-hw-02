f9303b6a514d208c528216d7ff1c7c438ea5285e
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2023-06-10 00:58:42 +0200

    correct

--- server.js
+++ server.js
@@ -1,24 +1,6 @@
-const express = require("express");
-const cors = require("cors");
+const app = require("./app");
 const mongoose = require("mongoose");
-
 require("dotenv").config();
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-const routerApi = require("./routes/api/contacts");
-app.use("./routes/api/contacts", routerApi);
-
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" })
-})
-
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
 
 const PORT = process.env.PORT || 3000;
 const uriDb = process.env.DB_HOST;
